4c843dfb2dfb178cbb8389b94ffa5b7c5688e241
Jenni Haslam <haslam.jenni@gmail.com>
2018-05-11 16:26:13 +0100

    small updates to profile

--- Yogging.Models/ViewModels/ProfileViewModel.cs
+++ Yogging.Models/ViewModels/ProfileViewModel.cs
@@ -1,37 +1,49 @@
-﻿namespace Yogging.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yogging.Models.ViewModels
 {
     public class ProfileViewModel
     {
         public int Id { get; set; }
 
+        [Display(Name = "Full name")]
         public string FullName { get; set; }
-
+        
+        [Display(Name = "Image url")]
         public string ImageUrl { get; set; }
 
         public string Blurb { get; set; }
 
+        [Display(Name = "Longer blurb")]
         public string LongerBlurb { get; set; }
 
+        [Display(Name = "Instagram username")]
         public string InstagramUsername { get; set; }
 
         public string InstagramUrl { get; set; }
 
+        [Display(Name = "Linked In username")]
         public string LinkedInUsername { get; set; }
 
         public string LinkedInUrl { get; set; }
 
+        [Display(Name = "Twitter Username")]
         public string TwitterUsername { get; set; }
 
         public string TwitterUrl { get; set; }
 
+        [Display(Name = "Github username")]
         public string GitHubUsername { get; set; }
 
         public string GitHubUrl { get; set; }
 
+        [Display(Name = "Blog url")]
         public string BlogUrl { get; set; }
 
+        [Display(Name = "Current job title")]
         public string CurrentJobTitle { get; set; }
 
+        [Display(Name = "Contact email address")]
         public string ContactEmailAddress { get; set; }
     }
 }
